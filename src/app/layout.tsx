9c1255a12226a3eb683b57fc6a7ef96be29e4f9c
AsifZaman777 <eramasif16@gmail.com>
2025-08-28 16:19:16 +0600

    favicon configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -151,10 +151,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="NeatXL" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="application-name" content="NeatXL" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/public/favicon-v2.ico" sizes="any" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={`${inter.className} bg-gray-50`}>
